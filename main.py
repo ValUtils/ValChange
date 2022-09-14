0f872cd4981e27d468f670b299e0465e6634871b
PWall <34860495+PWall2222@users.noreply.github.com>
2022-09-14 15:56:28 +0200

    refactor: move valorant launch to separate file

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-
 from infi.systray import SysTrayIcon
-from switch import switch_user, restore_user
-from proc import wait_process_close, wait_process_open
 from os import _exit
 
-from .structs import ChangeUser
-from .options import set_options, restore_options
 from .config import get_config
+from .launch import valorant_start
 
 def create_tray(user):
 	def on_quit_callback(systray: SysTrayIcon):
@@ -15,18 +11,6 @@ def create_tray(user):
 	systray = SysTrayIcon("explorer", "ValChange", menu_options, on_quit=on_quit_callback)
 	systray.start()
 	return systray
-
-def valorant_start(cUser: ChangeUser):
-	set_options(cUser)
-
-	switch_user(cUser.user)
-
-	wait_process_open("VALORANT.exe")
-	wait_process_close("VALORANT.exe")
-
-	restore_user()
-
-	restore_options(cUser)
 
 def main():
 	cUser = get_config()
