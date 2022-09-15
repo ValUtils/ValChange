8b8a0864a49b52144805a2b14559741090d0ab85
PWall <34860495+PWall2222@users.noreply.github.com>
2022-09-15 21:35:18 +0200

    refactor: use relative imports

--- launch.py
+++ launch.py
@@ -1,8 +1,7 @@
-from switch import switch_user, restore_user
-from proc import wait_process_close, wait_process_open
-
 from .structs import ChangeUser
+from .switch import switch_user, restore_user
 from .options import set_options, restore_options
+from .proc import wait_process_close, wait_process_open
 
 def valorant_start(cUser: ChangeUser):
 	set_options(cUser)
