1cc1a9c44afed8ff8b9d671dcebfeec7d1dbd333
PWall <34860495+PWall2222@users.noreply.github.com>
2023-04-19 21:37:11 +0200

    feat: launch valorant without separate launcher

--- ValChange/launch.py
+++ ValChange/launch.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 
-from .subproc import run, run_fn, runs
+from .subproc import run, run_fn, runs, subrun
 from .config import switcher_read
 from .structs import ChangeUser, Program, Programs
 from .switch import switch_user, restore_user
 from .options import set_options, restore_options
 from .proc import kill_all, wait_process_close, wait_process_open
+from .product import get_riot_installs
 
 
 def valorant_start(cUser: ChangeUser):
@@ -38,10 +39,24 @@ def get_programs():
     return programs
 
 
+def riot_launcher():
+    installs = get_riot_installs()
+    client_path = Path(installs["rc_default"])
+    args = "--launch-product=valorant --launch-patchline=live"
+    subrun(f'"{client_path}" {args}')
+
+
+def valorant_launcher(programs: Programs):
+    if programs.launcher:
+        run(programs.launcher)
+        return
+    run_fn(riot_launcher)
+
+
 def launch_valorant():
     programs = get_programs()
     runs(programs.beforeLaunch)
-    run(programs.launcher)
+    valorant_launcher(programs)
     wait_process_open("VALORANT.exe")
     runs(programs.afterLaunch)
     wait_process_close("VALORANT.exe")
