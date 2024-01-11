dc7a0b5b26d5f207d76e8312ea0d09037348fcc5
PWall <34860495+PWall2222@users.noreply.github.com>
2024-01-11 20:04:14 +0100

    feat: add main to init as launch

--- ValChange/__init__.py
+++ ValChange/__init__.py
@@ -1,6 +1,6 @@
 from .config import get_config
-from .main import change
+from .main import change, main as launch
 
 __all__ = [
-    "change", "get_config"
+    "change", "get_config", "launch"
 ]
